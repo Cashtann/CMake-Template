0ad7bb50bc20e60304b3ca63af45e4941e14c741
Maciek <121943141+Cashtann@users.noreply.github.com>
2024-06-03 14:15:17 +0200

    Add files via upload
    bigger update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-cd build
+cd bin/Linux/Debug  
+# directory, where the executable file is located, may differ if compiling in Release mode 
+# or Debug mode, also depends on OS
 ./run
 cd ..
