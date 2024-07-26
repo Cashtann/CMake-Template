650f7c63923dbead60d6a9d6e034e21f96d07ea1
Maciek <121943141+Cashtann@users.noreply.github.com>
2024-07-26 21:38:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cd build
 make
 cd ..
 
-cd bin/Linux/Debug  
+cd bin/Debug  
 # directory, where the executable file is located, may differ if compiling in Release mode 
 # or Debug mode, also depends on OS
 ./run
